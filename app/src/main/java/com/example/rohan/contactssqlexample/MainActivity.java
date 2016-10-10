67ad9e322bde580e80cde7fd12332e0e8922c590
Rohan Anand <thededlier@gmail.com>
2016-10-10 15:28:47 +0530

    Successfully counting contacts

--- app/src/main/java/com/example/rohan/contactssqlexample/MainActivity.java
+++ app/src/main/java/com/example/rohan/contactssqlexample/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.rohan.contactssqlexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView contactCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        contactCount = (TextView)findViewById(R.id.contactCount);
+
+        int count = db.getContactsCount();
+        contactCount.setText("Contacts : " + count);
+        List<Contact> contacts = db.getAllContacts();
+
     }
 }
