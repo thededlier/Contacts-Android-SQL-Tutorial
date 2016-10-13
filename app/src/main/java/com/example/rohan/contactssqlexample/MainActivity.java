e87f093bfdfc7c518ddb7f94c61d170899f42716
Rohan Anand <thededlier@gmail.com>
2016-10-13 14:26:34 +0530

    cosmetic changes and onClickListeners

--- app/src/main/java/com/example/rohan/contactssqlexample/MainActivity.java
+++ app/src/main/java/com/example/rohan/contactssqlexample/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rohan.contactssqlexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     TextView contactCount;
     TextView contactList;
+
+    Button btnAdd;
+    Button btnRemove;
+    Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +30,39 @@ public class MainActivity extends AppCompatActivity {
         contactList     = (TextView) findViewById(R.id.contactList);
 
 
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnRemove = (Button)findViewById(R.id.btnRemove);
+        btnUpdate = (Button)findViewById(R.id.btnUpdate);
+
         int count = db.getContactsCount();
         contactCount.setText("Contacts : " + count);
 
         String list = "";
         List<Contact> contacts = db.getAllContacts();
         for(Contact cn : contacts) {
-            list += "Id : " + cn.getID() + " Name : " + cn.getName() + " Phone Number : " + cn.getPhoneNumber();
+            list += "Id : " + cn.getID() + " Name : " + cn.getName() + " Phone Number : " + cn.getPhoneNumber() + "\n";
         }
-
         contactList.setText(list);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
